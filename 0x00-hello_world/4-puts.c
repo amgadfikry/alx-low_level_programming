55eacef1b412654e6272a283cfc5f38ba84c4417
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-16 05:41:24 -0800

    ghfjhgj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /*
- *function to puts outpt
- * 
- *return 0
+ * main - entery point
  *
- *  */
+ * return: always 0 (success)
+ */
 int main(void)
 {
-	puts( "\"Programming is like building a multilingual puzzle" );
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
