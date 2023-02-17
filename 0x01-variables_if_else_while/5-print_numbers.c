71a45b7419dc80c6797945accfbf63c1290d9e29
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-17 09:56:04 -0800

    5task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry main function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		printf("%d",c);
+	}
+	printf('\n');
+
+	return (0);
+}
+
