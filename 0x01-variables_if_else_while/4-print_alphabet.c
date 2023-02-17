d68e3532eb1001aa4c83ac9472ec0ca6710d13ee
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-17 09:49:34 -0800

    4task

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry main function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' || c != 'e')
+			putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
