50ee82e8432214ee508cc5c5bef6ddf2a78f6829
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-17 10:00:28 -0800

    6task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry main function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
