3abb2f3114f9e392d3f482451faec560db865a5c
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-17 09:33:27 -0800

    2task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry main function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c < 'z'; c++)
+		putchar(c);
+		putchar('\n');
+
+	return (0)
+}
+
