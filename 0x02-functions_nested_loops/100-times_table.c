11d604d241ee3108784cc2d8c2d032db698b9bac
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-21 15:01:16 -0800

    100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main"
+/**
+ * print_times_table - function print table of numbers
+ * @n: input number
+ * Return: nothing
+ */
+void print_times_table(int n)
+{
+	int i, j, num;
+
+	if ( n > 15 || n < 0)
+		continue;
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				num = i * j;
+				if ((num / 10) == 0)
+				{
+					if (j != 0)
+						_putchar(' ');
+					_putchar(num + '0');
+					if (j == n)
+						continue;
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((num / 10) + '0');
+					_putchar((num % 10) + '0');
+					if (j == n)
+						continue;
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
