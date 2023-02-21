0d79417b5c7c676b0f464d30f102a1e31f176a4d
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-21 12:11:13 -0800

    7task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - get last digit of number
+ * @n: input int
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	double num;
+	int result;
+
+	num = n / 10;
+	result = (num - (int)num) * 10;
+
+	return (result);
+}
