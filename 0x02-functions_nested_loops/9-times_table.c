4dfc54951fcc0d0122defe10761d466387865da9
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-21 13:43:53 -0800

    9task

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * times_table - write table
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int i;
+	int start = 0;
+	int j = 10;
+	int num;
+
+	for (i = 0; < 10; i++)
+	{
+		num = 0;
+		_putchar(num);
+		_putchar(',');
+		while (j >= 0)
+		{
+			num += i;
+			_putchar(num);
+			_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
