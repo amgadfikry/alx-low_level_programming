cdf3a490b697f5af92eff1681088bf09241762e4
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-21 10:15:40 -0800

    3task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
  * _islower - to determine char is lowercase or not
+ * @c: input char
  * Return: 1 if lower and 0 if upper
  */
 int _islower(int c)
 {
-	int result;
+	int result = 0;
 	char n;
 
 	for (n = 'a'; n <= 'z'; n++)
 	{
 		if (n == c)
 			result = 1;
-		else
-			result = 0;
 	}
 
 	return (result);
