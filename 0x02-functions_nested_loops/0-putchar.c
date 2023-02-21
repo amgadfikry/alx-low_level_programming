6e7de69eb78b2757ae23a463bbd5c950511ce04a
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-21 09:54:14 -0800

    sadasdas

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,11 +4,11 @@
  * Return: 0
  */
 int main(void)
-{	
+{
 	char printed[8] = "_putchar";
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 8; i++)
 		_putchar(printed[i]);
 	_putchar('\n');
 
