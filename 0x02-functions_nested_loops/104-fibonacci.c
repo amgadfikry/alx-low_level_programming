2a353557dfb5860dc1fda57e58e42dca2e07da08
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-21 16:02:55 -0800

    104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - entery function
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	long int a = 1;
+	long int b = 2;
+
+	printf("%ld, ", a);
+	printf("%ld, ", b);
+	for (i = 1; i <= 96; i++)
+	{
+		if (i % 2 == 1)
+		{
+			a = a + b;
+			printf("%ld, ", a);
+		}
+		else if (i % 2 == 0)
+		{
+			b = a + b;
+			if (i == 96)
+				printf("%ld", b);
+			else
+				printf("%ld, ", b);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
