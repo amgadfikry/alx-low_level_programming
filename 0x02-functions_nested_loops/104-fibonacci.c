74f8ef801315c4d454eaa024f0c8be1414c892c5
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-21 16:29:23 -0800

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,22 +6,22 @@
 int main(void)
 {
 	int i;
-	unsigned long long int a = 1;
-	unsigned long long int b = 2;
+	long int a = 1;
+	long int b = 2;
 
-	printf("%lu, ", a);
-	printf("%lu, ", b);
+	printf("%ld, ", a);
+	printf("%ld, ", b);
 	for (i = 1; i <= 96; i++)
 	{
 		if (i % 2 == 1)
 		{
 			a = a + b;
-			printf("%lu", a);
+			printf("%ld", a);
 		}
 		else if (i % 2 == 0)
 		{
 			b = a + b;
-			printf("%lu", b);
+			printf("%ld", b);
 		}
 		if (i != 96)
 			printf(", ");
