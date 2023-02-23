aad8704a3ede4c8b00ac538fb3f884a005312233
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-23 08:27:11 -0800

    6task

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,16 +2,17 @@
 /**
  * print_line - draw straight line
  * @n: num input
+ *
  * Return: nothing
  */
 void print_line(int n)
 {
-	int i;
+	int a;
 
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
-			_putchar(95);
+		for (a = 1; a <= n; i++)
+			_putchar('_');
 	}
 	_putchar('\n');
-}	
+}
