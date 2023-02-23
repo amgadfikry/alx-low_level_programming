40c1c42772b2cb392ddf49d1ad1eda7feca2fe87
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-23 07:05:01 -0800

    5task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,8 +10,11 @@ void more_numbers(void)
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
-			_putchar(j);
+		{
+			if (j > 9)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
