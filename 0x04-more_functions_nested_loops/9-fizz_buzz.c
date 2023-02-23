f1486b9c946ff4d83e73b32e7ef60688cb8c0ce1
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-23 09:18:27 -0800

    9task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
@@ -26,7 +26,7 @@ int main(void)
 			printf("%d", i);
 		}
 
-		if (i != 99)
+		if (i != 100)
 		{
 			printf(" ");
 		}
