ecae2077afdcfc9192c1afa26beaf36086366ea7
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-23 06:13:15 -0800

    1isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - know if input digit or not
+ * @c: input digit
+ * Return: 1 if digit and 0 if not
+ */
+int _isdigit(int c)
+{
+	int i;
+
+	for (i = 0; i ,= 9; i++)
+	{
+		if (c == i)
+			return (1);
+	}
+
+	return (0);
+}
