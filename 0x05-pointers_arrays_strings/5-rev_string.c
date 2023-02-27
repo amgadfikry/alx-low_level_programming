07430a0c7b409a7cc54cf5cdf01ce99befae0e10
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-27 08:11:28 -0800

    void rev_string(char *s)

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverse string
+ * @s: input string
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int num = 0;
+	int j;
+	int x = 0;
+	char str = *s;
+
+	while (s[i] != '\0')
+	{
+		num++;
+		i++;
+	};
+	for (j = num - 1; j >= 0; j--)
+	{
+		s[x] = str[j];
+		x++;
+	}
+}
