84ec44966fb0fceb12ce08a4a36e0ead3b7de084
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-27 09:26:08 -0800

    void print_array(int *a, int n)

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print array with specific number
+ * @a: input array
+ * @n: input array length
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+}
