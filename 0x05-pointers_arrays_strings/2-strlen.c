8504e17c95126705adfa4ff2511090ecdeb403f1
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-27 07:44:05 -0800

    int _strlen(char *s)

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * strlen - return length of string
+ * _strlen - return length of string
  * @s: input char
  * Return: length
  */
 int _strlen(char *s)
 {
-	int i= 0;
+	int i = 0;
 	int num = 0;
 
 	while (s[i] != '\0')
