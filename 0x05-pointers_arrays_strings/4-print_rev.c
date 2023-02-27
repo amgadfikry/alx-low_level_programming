f98d3b0b109ae82922f47f515f6cd1a23f08da7a
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-27 08:01:42 -0800

    void print_rev(char *s)

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - return length of string
+ * @s: input string
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int num = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		num++;
+		i++;
+	};
+	for (j = num; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
