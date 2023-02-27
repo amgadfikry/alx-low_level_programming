8045ffca1491a66be304c985781603ae11770c3c
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-27 07:24:32 -0800

    void reset_to_98(int *n)

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - write a new value
+ * @n: input pointer
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,2 @@
+int _putcahr(char c);
+void reset_to_98(int *n);
