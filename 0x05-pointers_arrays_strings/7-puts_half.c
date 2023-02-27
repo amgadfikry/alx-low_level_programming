67d686c4f44f211026cb9bbcca65decdbe933dc2
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-02-27 09:11:57 -0800

    void puts_half(char *str)

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - print second half of string
+ * @str: input char
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int num = 0;
+	int n, j;
+
+	while (s[i] != '\0')
+	{
+		num++;
+		i++;
+	}
+	if (num % 2 == 0)
+		n = num / 2;
+	else
+		n = (num - 1) / 2;
+	for (j = n; j > num; j++)
+		_putchar(str[j]);
+	_putchar('\n');
+}
