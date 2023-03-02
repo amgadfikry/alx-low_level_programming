2b7322f093a630c633483c7d9b7d473a2c5c136d
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-02 14:45:09 -0800

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * infinite_add - add two number
+ * @n1: input num
+ * @n2: input num
+ * @r: result
+ * @size_r: size of result
+ * Return: added number
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (r);
+}
