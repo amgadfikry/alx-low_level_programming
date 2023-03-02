2378883615cf8f595f8e8204c2cd7e71fbfc9925
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-02 14:51:43 -0800

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_buffer - print buffer
+ * @b: content
+ * @size: input size
+ * @Return: nothing
+ */
+void print_buffer(char *b, int size)
+{
+	printf("nothing");
+}
