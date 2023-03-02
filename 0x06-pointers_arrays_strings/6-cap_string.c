2f5eb9d2e1aafaa8bdb1649dde63515145609230
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-02 10:12:29 -0800

    tabulation

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * cap_string - capitalize string
+ * @s: input string
+ * Return: capitalize
+ */
+char *cap_string(char *s)
+{
+	char sep[12] = {' ','\n',',',';','.','!','?','"',')','(','{','}'};
+	char *c = s;
+	int i, num = 0;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		*s -= 32;
+	while (*s)
+	{
+		for (i = 0; i < 12; i++)
+		{
+			if ((*s >= 'a' && *s <= 'z') && *(s - 1) == sep[i])
+			{
+				*s -= 32;
+			}
+		}
+		s++;
+	}
+
+	return (c);
+}
