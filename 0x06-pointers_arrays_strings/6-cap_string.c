7d84c3d4eb1043114dd5f7e6d6e116cdb6afe399
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-02 10:34:26 -0800

    tabulation

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,15 +7,15 @@
 char *cap_string(char *s)
 {
 	char sep[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', ')', '(', '{', '}'};
-	int i = 0;
+	int i = 0, x;
 
 	while (s[i] != '\n')
 	{
 		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
 			s[i] -= 32;
-		for (i = 0; i < 13; i++)
+		for (x = 0; x < 13; x++)
 		{
-			if ((s[i] >= 'a' && s[i] <= 'z') && s[i -1] == sep[i])
+			if ((s[i] >= 'a' && s[i] <= 'z') && s[i -1] == sep[x])
 				s[i] -= 32;
 		}
 		i++;
