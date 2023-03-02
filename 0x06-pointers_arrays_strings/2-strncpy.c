da2787a10227c2815f9e657886a4c1c546275672
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-02 07:31:20 -0800

    char *_strncpy(char *dest, char *src, int n)

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,20 +8,35 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int num_src = 0;
-	int j = 0, x;
+	int num_src = 0, num_dest = 0;
+	int j = 0, i = 0, x;
 
+	while (dest[i] != '\0')
+	{
+		i++;
+		num_dest++
+	}
 	while (src[j] != '\0')
 	{
 		j++;
 		num_src++;
 	}
 	if (n > num_src)
-		n = num_src;
-
-	for (x = 0; x < n; x++)
 	{
-		dest[x] = src[x];
+		n = num_src;
+		for (x = 0; x < n; x++)
+		{
+			dest[x] = src[x];
+		}
+	}
+	else if (num_src == 0)
+	{
+		dest[num_dest] = '\0';
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+			dest[x] =src[x];
 	}
 
 	return (dest);
