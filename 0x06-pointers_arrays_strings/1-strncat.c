a970dd878dc0238e0b4d80feef5f09fe0a9a8ddb
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-02 06:06:30 -0800

    char *_strncat(char *dest, char *src, int n)

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,7 +10,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int num_dest = 0;
 	int num_src = 0;
-	int i = 0, j = 0; y = 0, x, z = n;
+	int i = 0, j = 0, y = 0, x, z = n;
 
 	while (dest[i] != '\0')
 	{
