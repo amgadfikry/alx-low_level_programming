97f150c72bd219c19f410048f9dfa345123237e0
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-02 07:56:04 -0800

    int _strcmp(char *s1, char *s2)

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcmp - compare strings
+ * @s1: input string
+ * @s2: input string
+ * Return: result of comparing
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		*s1++;
+		*s2++;
+	}
+
+	return (0);
+}
