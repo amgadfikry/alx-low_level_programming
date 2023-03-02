c04fa955be10c9e7752523a5f9b47f369ef05add
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-02 07:06:30 -0800

    char *_strncpy(char *dest, char *src, int n)

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strncpy - concate two string
+ * @dest: input str
+ * @src: input str
+ * @n: input int
+ * Return: pointer to result
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int num_dest = 0, num_src = 0;
+	int i = 0, j = 0, y = 0, x;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+		num_dest++;
+	}
+	while (src[j] != '\0')
+	{
+		j++;
+		num_src++;
+	}
+	if (n > num_src)
+		n = num_src;
+
+	for (x = 0; x < n; x++)
+	{
+		dest[x] = src[x];
+	}
+
+	return (dest);
+}
