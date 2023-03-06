7e051160e980f39e3b4810d664ad12107404a84d
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-06 10:00:07 -0800

    char *_strstr(char *haystack, char *needle)

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - locate substring
+ * @haystack: input string
+ * @needle: input string
+ * Return: pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int go = 0;
+	char *c = needle;
+
+	while (*haystack)
+	{
+		if (*haystack == neddle[0])
+		{
+			while (*needle)
+			{
+				if (*haystack == *needle)
+					go = 1;
+				else
+				{
+					go = 0;
+					break;
+				}
+				needle++;
+			}
+		}
+		haystack++;
+		needle = c;
+		if (go == 1)
+			return (haystack);
+	}
+
+	return (NULL);
+}
