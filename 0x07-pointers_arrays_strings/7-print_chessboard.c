4546cbd96a0885201a175a5b818267a8bfe631f2
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-06 12:07:53 -0800

    kljljklj

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - print board
+ * @a: input array
+ * Return: nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x = 0, y = 0;
+
+	while (x < 8)
+	{
+		while (y < 8)
+		{
+			_putchar(a[x][y]);
+		}
+		_putchar('\n');
+	}
+}
