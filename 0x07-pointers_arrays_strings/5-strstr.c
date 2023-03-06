f2a3043057923f0eacef805c85cb2cb73b292055
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-06 11:54:15 -0800

    asdasdasd

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,30 +8,21 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0;
-	int go = 0;
 	char *c = needle;
+	char *p = haystack;
 
 	while (*haystack)
 	{
-		if (*haystack == needle[0])
-		{
-			while (*needle)
-			{
-				if (*haystack == *needle)
-					go = 1;
-				else
-				{
-					go = 0;
-					break;
-				}
-				needle++;
-			}
-		}
-		haystack++;
 		needle = c;
-		if (go == 1)
+		p = haystack;
+		while (*haystack == *needle)
+		{
+			haystack++;
+			needle++;
+		}
+		if (*needle == '\0')
 			return (haystack);
+		haystack = p + 1;
 	}
 
 	return (NULL);
