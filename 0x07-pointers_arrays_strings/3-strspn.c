e3e962c0963435086853841a698fb9397bd99242
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-06 06:41:03 -0800

    unsigned int _strspn(char *s, char *accept)

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strspn - get length of prefix
+ * @s: input string
+ * @accept: input 
+ * Return: length
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+
+	while (*s != '\n')
+	{
+		while (*accept)
+		{
+			if (*s == *accept)
+				i++;
+			accept++;
+		}
+		s++;
+	}
+	
+	return (i);
+}
