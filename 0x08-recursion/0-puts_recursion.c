7362245a1ea40a93c2c9d910b64d52ef75826e82
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-08 08:44:47 -0800

    void _puts_recursion(char *s)

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _put_recursion - print string follow with new line
+ * @s: input string
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		_putchar('\n');
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
