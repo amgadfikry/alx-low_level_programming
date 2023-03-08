abd22e3414e37a2e2aff85fb7d88b859cebcc042
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-08 12:26:20 -0800

    int wildcmp(char *s1, char *s2)

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * wildcmp - compare strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 or 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 && *s2)
+		return (0);
+}
