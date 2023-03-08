089ead4a3886bdce5d401058cd323cfa5299ab29
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-08 10:29:23 -0800

    _sqrt_recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - give square root
+ * @n: input number
+ * Return: number
+ */
+int sqr(int start, int result)
+{
+	if (start * start = result)
+		return (start)
+	else if (start < 0)
+		return (-1);
+	else
+		sqr(start - 1, result);
+}
+int _sqrt_recursion(int n)
+{
+	return (sqr(n, n));
+}
