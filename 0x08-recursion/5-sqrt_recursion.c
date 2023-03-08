3e70c1749c82bc102f444a2786ffe85952905e99
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-08 11:07:56 -0800

    _sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,6 @@
 #include "main.h"
-/**
- * sqr - function check root
- * @start: input number
- * @result: input number
- * Return: result
- */
 int sqr(int start, int result);
+
 /**
  * _sqrt_recursion - give square root
  * @n: input number
@@ -16,6 +11,12 @@ int _sqrt_recursion(int n)
 	return (sqr(1, n));
 }
 
+/**
+ * sqr - check root
+ * @start: input number
+ * @result: input number
+ * Return: result int
+ */
 int sqr(int start, int result)
 {
 	if (start * start == result)
