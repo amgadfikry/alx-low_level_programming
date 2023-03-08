30781812697c7639440f8f2f722f692c1b28b990
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-08 10:31:37 -0800

    _sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,13 +7,13 @@
 int sqr(int start, int result)
 {
 	if (start * start == result)
-		return (start)
-	else if (start < 0)
+		return (start);
+	else if (start > result)
 		return (-1);
 	else
-		sqr(start - 1, result);
+		sqr(start + 1, result);
 }
 int _sqrt_recursion(int n)
 {
-	return (sqr(n, n));
+	return (sqr(1, n));
 }
