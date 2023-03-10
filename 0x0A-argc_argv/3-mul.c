7496a272e542afbe75f879b5e8a5c8e6be023a6e
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-10 09:08:13 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,16 +8,22 @@
  */
 int main(int argc, char *argv[])
 {
-	int x, int y;
+	int x;
+	int num1, num2;
+	char i, y;
 
 	if (argc == 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	x = argv[1];
+	i = argv[1];
 	y = argv[2];
-	printf("%d\n", x * y);
+	for (x = 0; x != '\0'; x++)
+		num1 = num1 * 10 + (i[x] - 48);
+	for (x = 0; x != '\0'; x++)
+		num2 = num2 * 10 + (y[x] - 48);
+	printf("%d\n", num1 * num2);
 
 	return (0);
 }
