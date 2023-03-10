8ba935e948ad74c809afa269e98641cfe4f28d3c
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-10 08:46:47 -0800

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,11 +8,10 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	
-	for (i = 0; i < argc; i++)
+	while (*argv)
 	{
-		printf("%s\n", argv[i]);
+		printf("%s\n", argv);
+		argv++;
 	}
 
 	return (0);
