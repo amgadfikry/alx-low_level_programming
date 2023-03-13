3cd3155d517298049a4085cce335c4802342a78f
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-13 14:53:08 -0700

    char *argstostr(int ac, char **av)

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * argstostr - concate all args of program
+ * @ac: length of args
+ * @av: array of args
+ * Return: concate of args
+ */
+char *argstostr(int ac, char **av)
+{
+	int size = 0, i, x, y;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (x = 0; av[i][x] != '\0'; x++)
+			size++;
+		size++;
+	}
+
+	str = malloc(sizeof(char) * size);
+
+	if (str == NULL)
+		return (NULL);
+
+	y = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (x = 0; av[i][x] != '\0'; x++)
+		{
+			str[y] = av[i][x];
+			y++;
+		}
+		str[y] = '\n';
+		y++;
+	}
+
+	return (str);
+}
