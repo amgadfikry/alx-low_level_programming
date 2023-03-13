9dcdfd7ad08a0be520d6a5ca178eaf7366bc27cc
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-13 16:11:53 -0700

    char **strtow(char *str)

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,7 +10,7 @@ char **strtow(char *str)
 {
 	char **arr;
 	char *s = str;
-	int size = 0;
+	int size = 0, word = 0, count = 0, y = 0;
 
 	if (str == NULL || !*str)
 		return (NULL);
@@ -30,5 +30,26 @@ char **strtow(char *str)
 
 	if (arr == NULL)
 		return (NULL);
+
+	while (*str)
+	{
+		if (*str != ' ')
+		{
+			word = 1;
+			count++;
+		}
+		if (*str != ' ' && (*(str + 1) == ' ' || *(str + 1) == '\0'))
+		{
+			word = 0;
+			y++;
+		}
+		if (word == 0 && count > 0)
+		{
+			count++;
+			arr[y] = malloc(sizeof(char) * count);
+			count = 0;
+		}
+	}
+
 	return (arr);
 }
