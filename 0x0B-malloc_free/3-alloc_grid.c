842d159d5f34a4d0c06f93556c63aed4867c77fa
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-13 10:37:56 -0700

    jhkjhjk

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,22 +9,31 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int *warr, i = 0, x = 0;
+	int **warr, i = 0, x = 0;
 	int *harr;
 
 	if (width == 0 || height == 0)
 		return (NULL);
-	warr = malloc(sizeof(int) * height);
+	*warr = malloc(sizeof(int) * height);
+	if (*warr == NULL)
+		return (NULL);
 	while (i < height)
 	{
-		harr = malloc(sizeof(int) * width);
-		while (x < width)
+		warr = malloc(sizeof(int) * width);
+		if (warr == NULL)
+			return (NULL);
+		i++;
+	}
+	i = 0;
+	while (i < height)
+	{
+		while ( x < width)
 		{
-			harr[x] = 0;
-			x++;
+			warr[i][x] = 0;
+			x++
 		}
-		warr[i] = *harr;
+		i++;
 	}
 
-	return (&warr);
+	return (warr);
 }
