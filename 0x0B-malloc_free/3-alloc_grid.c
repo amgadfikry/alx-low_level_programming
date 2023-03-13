6f89fa86643f5494b4b73ca7c5b5ff7f71b84fad
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-13 11:08:41 -0700

    jhkjhjk

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 
 	if (width == 0 || height == 0)
 		return (NULL);
-	*warr = malloc(sizeof(int) * height);
+	warr = malloc(sizeof(int) * height);
 	if (*warr == NULL)
 		return (NULL);
 	while (i < height)
