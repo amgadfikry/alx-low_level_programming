60b132e52776fdc8187d12c4feeb580f620b2546
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-16 14:01:23 -0700

    void *_calloc(unsigned int nmemb, unsigned int size)

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * calloc - allocate memory for array
+ * @nmemb: input int
+ * @size: input int
+ * Return: nothing
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+}
