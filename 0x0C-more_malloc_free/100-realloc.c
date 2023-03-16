624ca06aed7f497b4e5a377e37e0adfcfc825d47
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-16 15:22:48 -0700

    void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,7 +22,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		p = malloc(new_size);
 		if (p ==NULL)
 			return (NULL);
-		return (P);
+		return (p);
 	}
 	p = malloc(new_size);
 	if (p == NULL)
