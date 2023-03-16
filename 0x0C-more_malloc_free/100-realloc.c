36573db882c701bf1309c1c4238473c8af03f496
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-16 15:18:56 -0700

    void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc - reallocates memory block
+ * @ptr: input pointer
+ * @old_size: input int
+ * @new_size: input int
+ * Return: new pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *p;
+	int i;
+
+	if (new_size == old_size)
+		return (p);
+	if (new_size == 0 && ptr != NULL)
+		return (NULL);
+	
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p ==NULL)
+			return (NULL);
+		return (P);
+	}
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i <= old_size; i++)
+	{
+		p[i] = *ptr;
+		ptr++;
+	}
+
+	return (p);
+}
