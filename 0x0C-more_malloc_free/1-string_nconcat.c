f34c28c453fa1a890c9e6bc3970e0b7c3039dc43
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-16 12:58:35 -0700

    char *string_nconcat(char *s1, char *s2, unsigned int n)

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * string_nconcat - concate teo strings
+ * @s1: input string
+ * @s2: input string
+ * @n: int of first s2
+ * Return: null or pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str1 = s1, *str2 = s2;
+	int size1 = 0, size2 = 0, i = 0;
+	char *p;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (*s1)
+	{
+		size1++;
+		s1++;
+	}
+	s1 = str1;
+	while (*s2)
+	{
+		size2++;
+		s2++;
+	}
+	s2 = str2;
+	if (n > size2)
+		n = size2;
+	p = malloc(sizeof(char) * (size1 + size2 + 1));
+
+	if (p == NULL)
+		return (NULL);
+
+	while (i < size1)
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	while (i < (size1 + size2))
+	{
+		p[i] = s2[i - size1];
+		i++;
+	}
+
+	return (p);
+}
