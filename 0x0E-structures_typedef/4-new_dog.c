0febc3e1931db5e66ba924d36b9bb5fe387f6f09
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-20 10:21:01 -0700

    function create new dog struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,25 @@
 #include "dog.h"
 #include <stdio.h>
 /**
+ * string_len - give length of string
+ * @s: string pointer
+ * Return: length
+ */
+int string_len(char *s)
+{
+	char *p = s;
+	int len = 0;
+
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+	s = p;
+
+	return (len);
+}
+/**
  * new_dog - create new structure
  * @name: pointer name
  * @age: pointer float
@@ -11,13 +30,33 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	char *cname = name, *cowner = owner;
+	char *cname, *cowner;
+	int i = 0;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return(NULL);
 	if (name == NULL || owner == NULL)
 		return(NULL);
+	cname = malloc(sizeof(char) * string_len(name) + 1);
+	if (cname == NULL)
+		return (NULL);
+	cowner = malloc(sizeof(char) * string_len(owner) + 1);
+	if (cowner == NULL)
+		return (NULL);
+	while (i < string_len(name))
+	{
+		cname[i] = name[i];
+		i++;
+	}
+	cname[i] = '\0';
+	i = 0;
+	while (i < string_len(owner))
+	{
+		cowner[i] = owner[i];
+		i++;
+	}
+	cowner[i] = '\0';
 	new_dog->name = cname;
 	new_dog->age = age;
 	new_dog->owner = cowner;
