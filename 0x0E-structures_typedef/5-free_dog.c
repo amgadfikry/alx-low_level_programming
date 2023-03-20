9eb4babc43b613c9962319ad71613587a7be4a15
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-20 10:40:21 -0700

    Function free dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -8,7 +8,12 @@
  */
 void free_dog(dog_t *d)
 {
-	free(d->owner);
-	free(d->name);
-	free(new_dog);
+	if (d != NULL)
+	{
+		if (d->owner != NULL)
+			free(d->owner);
+		if (d->name != NULL)
+			free(d->name);
+		free(d);
+	}
 }
