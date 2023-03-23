18715ba3e2240b7abedd8db1b4c275475253fb8f
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-23 15:32:27 -0700

    fix function

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,6 +3,49 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
+ * p_char - print char
+ * @p: va_list
+ * Return: nothing
+ */
+void p_char(va_list p)
+{
+	printf("%c", va_arg(p, int));
+}
+/**
+ * p_int - print int
+ * @p: va_list
+ * Return: nothing
+ */
+void p_int(va_list p)
+{
+	printf("%d", va_arg(p, int));
+}
+/**
+ * p_float - print float
+ * @p: va_list
+ * Return: nothing
+ */
+void p_float(va_list p)
+{
+	printf("%f", va_arg(p, double));
+}
+/**
+ * p_str - print str
+ * @p: va_list
+ * Return: nothing
+ */
+void p_str(va_list p)
+{
+	char *str = va_arg(p, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+/**
  * print_all - function print all parameters different data type
  * @format: input list type of format
  * @...: rest parameters
@@ -10,6 +53,34 @@
  */
 void print_all(const char * const format, ...)
 {
+	fmt form[] = {
+		{"c", p_char},
+		{"i", p_int},
+		{"f", p_float},
+		{"s", p_str},
+		{NULL, NULL}
+	};
+	unsigned int i = 0, j;
+	char *s = "";
+	va_list p;
 
+	va_start(p, format);
+	while (format[i] != '\0')
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == *(form[j].c))
+			{
+				printf("%s", s);
+				form[j].f(p);
+				s = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(p);
 	printf("\n");
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,4 +5,15 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+#include <stdarg.h>
+/** 
+ * struct fmt - Struct fmt
+ * @c: The identifier
+ * @f: function pointer
+ */
+typedef struct fmt
+{
+	char *c;
+	void (*f)(va_list);
+} fmt;
 #endif
