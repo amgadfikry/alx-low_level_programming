57b6c5fc5a9423c93182f95debe98b61c1f98547
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-23 02:17:03 -0700

    Function print all number parameters

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_numbers - function print numbers
+ * @separator: input char
+ * @n: input number
+ * @...: rest parameters
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list p;
+
+	va_start(p, n);
+	while (i < n)
+	{
+		printf("%d", va_arg(p, int));
+		if (separator == NULL)
+			printf(" ");
+		else
+		{
+		if (i != n - 1)
+			printf("%c ", separator);
+		}
+	}
+	va_end(p);
+	printf("\n");
+}
