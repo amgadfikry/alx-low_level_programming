3e749a96389a72238f846b9b91ef7007e251ba4e
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-22 18:20:47 -0700

    Add all file of function that make operation to two numbers accord to sign

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,13 @@
 int main(int argc, char *argv[])
 {
 	int (*func_p)(int, int);
-
+	
+	if (argc != 4 && (atoi(argv[2]) != 0))
+	{
+		func_p = get_op_func("*");
+		printf("%d\n", func_p(atoi(argv[1]), atoi(argv[argc - 1])));
+		return (0);
+	}
 	if (argc != 4)
 	{
 		printf("Error\n");
