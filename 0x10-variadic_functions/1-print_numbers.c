bf25773c644f83a30dfa1bfb5609fc4dcda337b7
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-23 02:19:47 -0700

    Function print all number parameters

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		else
 		{
 		if (i != n - 1)
-			printf("%c ", separator);
+			printf("%s ", separator);
 		}
 	}
 	va_end(p);
