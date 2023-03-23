0fa2d56bc1a53973bc32920550df6fbcf5334778
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-23 03:30:56 -0700

    print Anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,15 @@
+#include "variadic_function.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - function print all parameters different data type
+ * @format: input list type of format
+ * @...: rest parameters
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+
+	printf("\n");
+}
