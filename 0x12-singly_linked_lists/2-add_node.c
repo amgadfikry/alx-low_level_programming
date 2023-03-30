8a25157c2e64aeeb66b8f3eef90e22a801ab5251
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-30 09:12:27 -0700

    add function add new node to list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 	int i = 0, j = 0;
 
 	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	if (new == NULL || str == NULL)
 		return (NULL);
 	while (str[i])
 	{
