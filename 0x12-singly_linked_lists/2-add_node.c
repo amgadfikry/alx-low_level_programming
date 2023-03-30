26e029ef2cc59e570ca9eb14079ff5819cb37381
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-30 08:19:59 -0700

    add function add new node to list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node - add new node
+ * @head: pointer to pointer of list
+ * @str: pointer to string
+ * Return: adress of element or NULL
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int i = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdub(str);
+	while (*str)
+	{
+		i++;
+		str++;
+	}
+	new->next = *head;
+	new->len = i;
+	*head = new;
+
+	return (*head);
+}
