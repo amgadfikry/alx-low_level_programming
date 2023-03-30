c6a23cf575a25411602e22d3d235af8dae10243f
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-03-30 11:06:29 -0700

    add free function

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - free list memory
+ * @head: list pointer
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *p;
+
+	p = head
+	while (p->next != NULL)
+	{
+		free(p->next);
+		p = p->next;
+	}
+	free(p);
+	free(head);
+}
