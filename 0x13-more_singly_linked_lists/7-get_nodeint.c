d865e893298c066b16c664a9ac7c32a3cf673a38
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-04-03 10:08:27 -0700

    remove extra brackets

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -24,6 +24,34 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 			return (ptr);
 		i++;
 		ptr = ptr->next;
+	nclude <stdlib.h>
+#include <stdio.h>
+	/**
+	 *  * get_nodeint_at_index - function get nide at specific index
+	 *   * @head: pointer to list
+	 *    * @index: index of node
+	 *     * Return: node
+	 *      */
+	listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+	{
+	        listint_t *ptr;
+	        unsigned int i = 0;
+	
+	        if (head == NULL)
+	        {
+		                free(head);
+		                return (NULL);
+		        }
+	        ptr = head;
+	        while (ptr != NULL)
+	        {
+		                if (inedx == i)
+		                        return (ptr);
+		                i++;
+		                ptr = ptr->next;
+		        }
+	
+	        return (NULL);
 	}
 
 	return (NULL);
