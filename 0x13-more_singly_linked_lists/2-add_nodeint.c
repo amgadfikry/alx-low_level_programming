aa9d0f19aefc771aef5853b8d802aa6a17683400
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-04-03 09:06:08 -0700

    fix betty description

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,6 +4,7 @@
  * add_nodeint - add node to begining of list
  * @head: list pointer start
  * @n: data int of node
+ * Return: pointer to node
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
