8ee0083c4bd1a7a2b6cbd51caa53b5e08a03a8b1
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-04-07 07:21:36 -0700

    change function to use bitwise operation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,67 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
-/**
- * check - check if number equal to binary base 2
- * @n: number check
- * @i: pow to raise 2
- * Return: 1 or 0
- */
-int check(unsigned long int n, int i)
-{
-	int pown = 1, num = n;
-
-	while (i > 0)
-	{
-		pown *= 2;
-		i--;
-	}
-	if (num - pown >= 0)
-		return (1);
-
-	return (0);
-}
-/**
- * get_max - get max number of this pow
- * @i: pow raise 2
- * Return: max number
- */
-unsigned long int get_max(int i)
-{
-	unsigned long int pown = 1;
-
-	while (i > 0)
-	{
-		pown *= 2;
-		i--;
-	}
-
-	return (pown);
-}
-/**
- * converter - convert number to array of binary
- * @n: number
- * @p: array of binary code
- * Return: pointer to array
- */
-int *converter(unsigned long int n, int p[])
-{
-	int i = 31;
-
-	while (i >= 0)
-	{
-		if (check(n, i) == 0)
-			p[i] = 0;
-		else if (check(n, i) == 1)
-		{
-			p[i] = 1;
-			n -= get_max(i);
-		}
-		i--;
-	}
-
-	return (p);
-}
 /**
  * flip_bits - number of bit you need to flip to change number
  * @n: first number
@@ -70,15 +8,15 @@ int *converter(unsigned long int n, int p[])
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int p1[32], p2[32], *first, *second, i = 0, differ = 0;
+	unsigned long int t;
+	int differ = 0, i = 0;
 
-	first = converter(n, p1);
-	second = converter(m, p2);
-
+	t = n ^ m;
 	while (i < 32)
 	{
-		if (first[i] != second[i])
+		if (t & 1)
 			differ++;
+		t = t >> 1;
 		i++;
 	}
 
