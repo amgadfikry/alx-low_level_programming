45000af44f759fe073455559a493dab714d1cdf3
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-04-07 06:07:02 -0700

    change function to use bitwise operators

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,21 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * power - get power of number 2
- * @n: pow number
- * Return: result of power
- */
-unsigned long int power(unsigned int n)
-{
-	unsigned long int sum = 1;
-	int i;
-
-	for (i = n; i > 0; i--)
-		sum *= 2;
-
-	return (sum);
-}
-/**
  * set_bit - set value of bit to 1 at given index
  * @n: number to change it
  * @index: index
@@ -23,26 +8,27 @@ unsigned long int power(unsigned int n)
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int i = 0, j = 0;
-	int p[32];
+	unsigned long int t = *n;
+	unsigned int i = 0;
 
-	if (index > 31)
-		return (-1);
-
-	while (i < 32)
+	if (*n == 0)
 	{
-		p[i] = *n % 2;
-		*n /= 2;
-		if (i == index)
-			p[i] = 1;
-		i++;
+		*n = *n | 1;
+		*n = *n << index;
+		return (*n);
 	}
 
-	while (j < 32)
+	while (i <= index)
 	{
-		if (p[j] == 1)
-			*n += power(j);
-		j++;
+		if (index == i)
+		{
+			*n = *n | 1;
+			t = t | *n;
+			*n = t;
+			return (*n);
+		}
+		*n = *n >> 1;
+		i++;
 	}
 
 	return (-1);
