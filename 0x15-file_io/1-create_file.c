579f37d1602572f8f9aebb106911eac5cd8afbd8
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-04-07 22:36:54 -0700

    fix NULL condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,11 +31,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	file = open(filename, O_RDWR | O_CREAT | O_RDWR | O_TRUNC);
 	if (file < 0)
-		return (1);
+		return (-1);
 	if (text_content == NULL)
 	{
 		close(file);
-		return (-1);
+		return (1);
 	}
 	wbytes = write(file, text_content, get_len(text_content));
 	if (wbytes < 0)
