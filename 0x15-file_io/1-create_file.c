3df02e0182987cfdbc8963bbbb4fd70a03be12b4
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-04-07 22:45:43 -0700

    change permission of created file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_RDWR | O_CREAT | O_RDWR | O_TRUNC);
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file < 0)
 		return (-1);
 	if (text_content == NULL)
