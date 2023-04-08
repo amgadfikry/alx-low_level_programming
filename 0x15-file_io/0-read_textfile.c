37184a178cb504d927efb77f4ddc9db6adfc0a5e
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-04-07 21:04:11 -0700

    function read file and print result to posix output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+/**
+ * read_textfile - function reads text file and print
+ * it to out put POSIX
+ * @filename: file name input
+ * @letters: test input
+ * Return: number of letters print or reads
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	size_t file, len, res;
+	char *str;
+
+	if (filename == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == 0)
+		return (0);
+	str = malloc(sizeof(char) * letters);
+	if (str == NULL)
+	{
+		free(str);
+		return (0);
+	}
+	len = read(file, str, letters);
+	res = write(STDOUT_FILENO, str, letters);
+	if (res < letters)
+		return (0);
+	close(file);
+
+	return (len);
+}
