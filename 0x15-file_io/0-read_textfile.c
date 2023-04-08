1fb667bdb14aba5e09010686d1bf870c36fe6052
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-04-07 21:14:00 -0700

    add free in cases before return

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,13 +14,13 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	size_t file, len, res;
+	int file, len, res;
 	char *str;
 
 	if (filename == NULL)
 		return (0);
 	file = open(filename, O_RDONLY);
-	if (file == 0)
+	if (file < 0)
 		return (0);
 	str = malloc(sizeof(char) * letters);
 	if (str == NULL)
@@ -29,10 +29,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	len = read(file, str, letters);
-	res = write(STDOUT_FILENO, str, letters);
-	if (res < letters)
+	if (len < 0)
+	{
+		free(str);
 		return (0);
+	res = write(STDOUT_FILENO, str, letters);
+	if (res < 0)
+	{
+		free(str);
+		return (0);
+	}
+	free(str);
 	close(file);
 
-	return (len);
+	return ((ssize_t)len);
 }
