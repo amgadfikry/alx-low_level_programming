7f7e9f55215d42f141890532b5c4ec1f388a25ad
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-04-07 22:35:24 -0700

    function that create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	file = open(filename, O_RDWR | O_CREAT | O_RDWR | O_TRUNC);
 	if (file < 0)
-		return (-1);
+		return (1);
 	if (text_content == NULL)
 	{
 		close(file);
