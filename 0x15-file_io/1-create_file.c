9c3c1a9bf6260b326bc115ef4937eccfe8cd505e
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-04-07 22:38:58 -0700

    add close file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -39,6 +39,7 @@ int create_file(const char *filename, char *text_content)
 	wbytes = write(file, text_content, get_len(text_content));
 	if (wbytes < 0)
 		return (-1);
+	close(file);
 
 	return (1);
 }
