aaa62ffd6ad291801eb606a367dbce6c2e7b91bc
Amgad Fikry Mohamed <66114771+amgadfikry@users.noreply.github.com>
2023-06-10 10:31:44 +0300

    create function that locate character in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strchr - locate character in string
+ * _strchr - locate a character in string
  * @s: string input
  * @c: char input
  * Return: location
