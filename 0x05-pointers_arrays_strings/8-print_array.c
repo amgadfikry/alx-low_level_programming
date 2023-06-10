0d28489cd87f11cce823c9c5ec68bf57bbfa0add
Amgad Fikry Mohamed <66114771+amgadfikry@users.noreply.github.com>
2023-06-10 09:54:46 +0300

    create function that print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_array - print array with specific number
+ * print_array - print array
  * @a: input array
  * @n: input array length
  * Return: nothing
